bcbfe753c963222915c888fb71a48345de897e91
Randy <RandyZeleznak@gmail.com>
2020-02-28 11:03:47 -0600

    Section#84 - addLog issue - 02/28/20:1100

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import M from 'materialize-css/dist/js/materialize';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const EditLogModal = () => {
   const [message, setMessage] = useState('');
